2f888bb22cb59c41cf3ea42a1211028701c8ed3e
Didahdx <didahdaniel@gmail.com>
2020-09-11 00:12:10 +0300

    added dialog box

--- app/src/main/java/com/didahdx/gadsleaderboard/presentation/activity/SubmitActivity.java
+++ app/src/main/java/com/didahdx/gadsleaderboard/presentation/activity/SubmitActivity.java
@@ -1,5 +1,6 @@
 package com.didahdx.gadsleaderboard.presentation.activity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.didahdx.gadsleaderboard.R;
 import com.didahdx.gadsleaderboard.data.network.GoogleForm;
@@ -68,25 +70,54 @@ public class SubmitActivity extends AppCompatActivity implements View.OnClickLis
                 return;
             }
 
-            IOExecutor.getInstance().execute(() -> {
-                GoogleForm.submit.submitProjects(
-                        email.getText().toString(),
-                        firstName.getText().toString(),
-                        lastName.getText().toString(),
-                        link.getText().toString()
-                ).enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+            // Create custom dialog object
+            final Dialog dialog = new Dialog(this);
+            // Include dialog.xml file
+            dialog.setContentView(R.layout.dialog_box);
+            dialog.setCancelable(true);
+            ImageButton close=(ImageButton) dialog.findViewById(R.id.button_cancel);
+            Button submit=(Button) dialog.findViewById(R.id.button_yes);
+            ConstraintLayout first=dialog.findViewById(R.id.constrain_confirm);
+            ConstraintLayout success=dialog.findViewById(R.id.constrain_success);
+            ConstraintLayout fail=dialog.findViewById(R.id.constrain_fail);
 
-                    }
+            submit.setOnClickListener(view1 -> {
+                IOExecutor.getInstance().execute(() -> {
+                    GoogleForm.submit.submitProjects(
+                            email.getText().toString(),
+                            firstName.getText().toString(),
+                            lastName.getText().toString(),
+                            link.getText().toString()
+                    ).enqueue(new Callback<ResponseBody>() {
+                        @Override
+                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                            if (response.isSuccessful()){
+                                first.setVisibility(View.GONE);
+                                success.setVisibility(View.VISIBLE);
+                                fail.setVisibility(View.GONE);
+                            }else{
+                                first.setVisibility(View.GONE);
+                                success.setVisibility(View.GONE);
+                                fail.setVisibility(View.VISIBLE);
+                            }
+                        }
 
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        @Override
+                        public void onFailure(Call<ResponseBody> call, Throwable t) {
+                            first.setVisibility(View.GONE);
+                            success.setVisibility(View.GONE);
+                            fail.setVisibility(View.VISIBLE);
+                        }
+                    });
 
-                    }
                 });
-
             });
+
+            close.setOnClickListener(view12 -> {
+                dialog.dismiss();
+            });
+
+            dialog.show();
         }
     }
 }
