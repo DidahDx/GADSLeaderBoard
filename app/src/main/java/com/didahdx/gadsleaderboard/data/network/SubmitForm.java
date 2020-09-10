59774de078573f02fe38ee5803d2519fab8cdd82
Didahdx <didahdaniel@gmail.com>
2020-09-10 23:24:17 +0300

    added submit to ui

--- app/src/main/java/com/didahdx/gadsleaderboard/data/network/SubmitForm.java
+++ app/src/main/java/com/didahdx/gadsleaderboard/data/network/SubmitForm.java
@@ -1,13 +1,18 @@
 package com.didahdx.gadsleaderboard.data.network;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface SubmitForm {
     @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
     @FormUrlEncoded
-    Call<Void> submitProjects(
-
+    Call<ResponseBody> submitProjects(
+            @Field("entry.1824927963") String emailAddress,
+            @Field("entry.1877115667") String name,
+            @Field("entry.2006916086") String lastName,
+            @Field("entry.284483984") String linkToProject
     );
 }
--- app/src/main/java/com/didahdx/gadsleaderboard/presentation/activity/SubmitActivity.java
+++ app/src/main/java/com/didahdx/gadsleaderboard/presentation/activity/SubmitActivity.java
@@ -1,16 +1,92 @@
 package com.didahdx.gadsleaderboard.presentation.activity;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.didahdx.gadsleaderboard.R;
+import com.didahdx.gadsleaderboard.data.network.GoogleForm;
+import com.didahdx.gadsleaderboard.util.IOExecutor;
 
-public class SubmitActivity extends AppCompatActivity {
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SubmitActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText firstName;
+    EditText lastName;
+    EditText email;
+    EditText link;
+    Button submit;
+    ImageButton buttonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+        collectIds();
+
+    }
+
+    private void collectIds() {
+        buttonBack = findViewById(R.id.buttonBack);
+        firstName = findViewById(R.id.editTextFirstName);
+        lastName = findViewById(R.id.editTextLastName);
+        email = findViewById(R.id.editTextTextEmailAddress);
+        link = findViewById(R.id.editTextLink);
+        submit = findViewById(R.id.btnSubmitRe);
+        submit.setOnClickListener(this);
+        buttonBack.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonBack) {
+            finish();
+        } else if (view == submit) {
+            if (TextUtils.isEmpty(firstName.getText())) {
+                firstName.setError("Can not be empty");
+                return;
+            }
+            if (TextUtils.isEmpty(lastName.getText())) {
+                lastName.setError("Can not be empty");
+                return;
+            }
+            if (TextUtils.isEmpty(email.getText())) {
+                email.setError("Can not be empty");
+                return;
+            }
+            if (TextUtils.isEmpty(link.getText())) {
+                link.setError("Can not be empty");
+                return;
+            }
+
+            IOExecutor.getInstance().execute(() -> {
+                GoogleForm.submit.submitProjects(
+                        email.getText().toString(),
+                        firstName.getText().toString(),
+                        lastName.getText().toString(),
+                        link.getText().toString()
+                ).enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                    }
+                });
+
+            });
+        }
     }
 }
